56222fe601a86670fb949f66972692fe840263c1
Facundo Noya <facunoya@hotmail.com>
2024-01-11 18:28:50 -0300

    create user POST

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -9,6 +9,14 @@ const userControllers = {
         const newUser = { ...req.body };
         newUser.money = parseInt(newUser.money, 10);
         console.log({ ...newUser });
+        fetch('http://localhost:3009/api/crearuno', {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(newUser)
+        })
+
         res.json(newUser);
     }
 
